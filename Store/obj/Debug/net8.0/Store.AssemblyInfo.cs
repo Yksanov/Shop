708da467cf8739d2875dc2da3b4edbed0ab0c5fb
Eldos Yksanov <eldosyksanov@gmail.com>
2024-10-14 22:47:09 +0600

    changes idea/config

--- Store/obj/Debug/net8.0/Store.AssemblyInfo.cs
+++ Store/obj/Debug/net8.0/Store.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Store")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+903ccf070e365aa63909f7cd8c2e1339f2198958")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5f0f640358e8e6c91c94000696392c1f7c559dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Store")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Store")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
