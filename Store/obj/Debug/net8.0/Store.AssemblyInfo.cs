43871e7f23171c215afb8d12b5535eda6221470d
Eldos Yksanov <eldosyksanov@gmail.com>
2024-10-27 22:15:31 +0600

    added link for Category and Brand

--- Store/obj/Debug/net8.0/Store.AssemblyInfo.cs
+++ Store/obj/Debug/net8.0/Store.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Store")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f0088d879b3f1c59db5c521b83792bdffc7cf5b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb13f5004718e753f1c471036908c722e9d62f43")]
 [assembly: System.Reflection.AssemblyProductAttribute("Store")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Store")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
