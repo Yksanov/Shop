ab3f5dc2d941bddb6e59f9d69f1c857dd20de15d
Eldos Yksanov <eldosyksanov@gmail.com>
2024-10-28 15:58:10 +0600

    change name Brand and Category to View Create.cshtml

--- Store/obj/Debug/net8.0/Store.AssemblyInfo.cs
+++ Store/obj/Debug/net8.0/Store.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Store")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bd878d1f5560ed78f652a584e48a12f1ceda813")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2377be01e3b20bfe08fff1dfe164ee8f642f681b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Store")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Store")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
