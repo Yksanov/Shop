69a6eb6e1e096cea333206322181d93cc8f90739
Eldos Yksanov <eldosyksanov@gmail.com>
2024-12-31 12:53:10 +0600

    changed Account View to Login and Register

--- Store/obj/Debug/net8.0/Store.AssemblyInfo.cs
+++ Store/obj/Debug/net8.0/Store.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Store")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+801927f2c65ac78fbee71cc88cdd0306e25cf7d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2476cd91442b61db8bb15381b0e9f9b1dd911252")]
 [assembly: System.Reflection.AssemblyProductAttribute("Store")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Store")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
