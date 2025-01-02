45f9832839a3b644751c424d800667056c5f90d5
Eldos Yksanov <eldosyksanov@gmail.com>
2025-01-02 18:19:38 +0600

    added new Repositories UserManager and implements IUserManager

--- Store/Repository/IUserRepository.cs
+++ Store/Repository/IUserRepository.cs
@@ -1,4 +1,5 @@
 using System.Security.Claims;
+using Microsoft.AspNetCore.Identity;
 using Store.Models;
 
 namespace Store.Repository;
@@ -6,4 +7,11 @@ namespace Store.Repository;
 public interface IUserRepository
 {
    public Task<MyUser?> GetUserAsync(ClaimsPrincipal user);
+   public Task<MyUser?> GetUserByIdAsync(string userId);
+   public Task<IdentityResult?> CreateUserAsync(MyUser user, string password);
+   public Task<IdentityResult?> UpdateUserAsync(MyUser user);
+   public Task<bool> IsInRoleAsync(MyUser user, string role);
+   public Task<MyUser?> FindByEmailAsync(string email);
+   public Task<MyUser?> FindByNameAsync(string userName);
+   public Task<IdentityResult?> AddRoleAsync(MyUser user, string role);
 }
--- Store/Repository/UserRepository.cs
+++ Store/Repository/UserRepository.cs
@@ -1,5 +1,6 @@
 using System.Security.Claims;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Store.Models;
 
 namespace Store.Repository;
@@ -16,5 +17,40 @@ public class UserRepository : IUserRepository
     public async Task<MyUser?> GetUserAsync(ClaimsPrincipal user)
     {
         return await _userManager.GetUserAsync(user);
+    }
+
+    public async Task<MyUser?> GetUserByIdAsync(string userId)
+    {
+        return await _userManager.FindByIdAsync(userId);
+    }
+
+    public async Task<IdentityResult?> CreateUserAsync(MyUser user, string password)
+    {
+        return await _userManager.CreateAsync(user, password);
+    }
+
+    public async Task<IdentityResult?> UpdateUserAsync(MyUser user)
+    {
+        return await _userManager.UpdateAsync(user);
+    }
+
+    public async Task<bool> IsInRoleAsync(MyUser user, string role)
+    {
+       return await _userManager.IsInRoleAsync(user, role);
+    }
+
+    public async Task<MyUser?> FindByEmailAsync(string email)
+    {
+        return await _userManager.FindByEmailAsync(email);
+    }
+
+    public async Task<MyUser?> FindByNameAsync(string userName)
+    {
+        return await _userManager.FindByNameAsync(userName);
+    }
+
+    public async Task<IdentityResult?> AddRoleAsync(MyUser user, string role)
+    {
+        return await _userManager.AddToRoleAsync(user, role);
     }
 }
